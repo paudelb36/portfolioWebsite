aeef52a9a041f5149d1e8c6ea4d38a5eb9ca1d8a
paudelb36 <paudelb11@gmail.com>
2024-03-28 18:06:44 +0545

    Commit!

--- script.js
+++ script.js
@@ -1,2 +1,21 @@
 
 
+  const navLinks = document.querySelectorAll('nav ul li a');
+
+  for (const link of navLinks) {
+    link.addEventListener('click', smoothScroll);
+  }
+
+  function smoothScroll(event) {
+    event.preventDefault();
+    const targetId = event.currentTarget.getAttribute('href');
+    const targetElement = document.querySelector(targetId);
+    const headerHeight = document.querySelector('nav').offsetHeight;
+
+    window.scrollTo({
+      top: targetElement.offsetTop - headerHeight,
+      behavior: 'smooth'
+    });
+  }
+
+
